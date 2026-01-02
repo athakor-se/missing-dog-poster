0c24121647fea6a65d60e6259878a0ccca632e1e
Aman Thakor <123771620+athakor-se@users.noreply.github.com>
2026-01-02 21:03:37 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,11 @@
+
+
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './src/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: []
+}).catch(err => console.error(err));
+
+// AI Studio always uses an `index.tsx` file for all project types.
